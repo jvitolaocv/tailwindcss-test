95853e5b4285b9ff5da364b0d75646eea5fc862e
zbedsole <zbedsole@myocv.com>
2020-04-27 14:19:49 -0500

    Restructured the way the dashboard looks using MUI icons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import React from 'react';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import BackupIcon from '@material-ui/icons/Backup';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import TwitterIcon from '@material-ui/icons/Twitter';
 import './App.css';
 
 export default function App() {
 	return (
 		<div className="App">
-			<div class="container">
-				<button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
-					Blue Button
-				</button>
-				<button class="btn btn-blue">Find Groups</button>
-				<div class="bg-red-100 border bordder-red-400 text-red-700 px-4 py-3 rounded relative my-5"role="alert">
-					<strong class="font-bold">Alert!</strong>
-					<span class="block sm:inline">Please update your password</span>
+			<div className="container">
+				<div className="bg-white max-w-xl mx-auto rounded-lg mt-4 mb-4 h-15">
+					<button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded m-5">
+						My Account
+					</button>
 				</div>
 			</div>
-			<div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
-				<div class="flex-shrink-0"></div>
+			<div className="max-w-4xl mx-auto flex flex-wrap p-6 bg-white rounded-lg shadow-xl justify-evenly content-center">
+				<div className="flex-shrink-0 bg-blue-500 p-16 border-4 rounded-lg m-4 h-48 w-48">
+					<div className="bg-white rounded-lg border-2 p-auto m-auto flex-col">
+						<SettingsIcon />
+						<h6 className="text-center font-serif text-xs tracking-wider antialiased">Settings</h6>
+					</div>
+				</div>
+				<div className="flex-shrink-0 bg-blue-500 py-16 px-16 border-4 rounded-lg m-4 h-48 w-48">
+					<div className="bg-white rounded-lg border-2 p-auto m-auto flex-col">
+						<ArrowForwardIosIcon />
+						<h6 className="text-center font-serif text-xs tracking-wide antialiased">Terminal</h6>
+					</div>
+				</div>
+				<div className="flex-shrink-0 bg-blue-500 py-16 px-16 border-4 rounded-lg m-4 h-48 w-48">
+					<div className="bg-white rounded-lg border-2 p-auto m-auto flex-col">
+						<BackupIcon />
+						<h6 className="text-center font-serif text-xs tracking-wider antialiased">Upload</h6>
+					</div>
+				</div>
+				<div className="flex-shrink-0 bg-blue-500 py-16 px-16 border-4 rounded-lg m-4 h-48 w-48">
+					<div className="bg-white rounded-lg border-2 p-auto m-auto flex-col">
+						<GetAppIcon />
+						<h6 className="text-center font-serif text-xs antialiased">Download</h6>
+					</div>
+				</div>
+				<div className="flex-shrink-0 bg-blue-500 py-16 px-16 border-4 rounded-lg m-4 h-48 w-48">
+					<div className="bg-white rounded-lg border-2 p-auto m-auto flex-col">
+						<VpnKeyIcon />
+						<h6 className="text-center font-serif text-xs tracking-wider antialiased">Encrypt</h6>
+					</div>
+				</div>
+				<div className="flex-shrink-0 bg-blue-500 py-16 px-16 border-4 rounded-lg m-4 h-48 w-48">
+					<div className="bg-white rounded-lg border-2 p-auto m-auto flex-col">
+						<TwitterIcon />
+						<h6 className="text-center font-serif text-xs tracking-wider antialiased">Social Media</h6>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
