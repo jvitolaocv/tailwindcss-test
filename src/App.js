0bdea8d1f6cce0592d4621366a98f5deac9efe19
zbedsole <zbedsole@myocv.com>
2020-05-04 17:00:41 -0500

    Slightly fixed the layout of the home screen

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ export default function App() {
 	
 	return (
 		<div className="container flex h-screen w-screen justify-center text-center">
-			<div className="w-screen h-screen mx-auto p-40 bg-blue-600 rounded-lg self-center text-center relative">
-				<div style={{top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className="h-full w-full bg-blue-500 inline-block mx-8 my-8 p-4 absolute flex-col items-center justify-center leading-8 border-2 border-blue-500 rounded-lg">
-					<label className="w-screen clearfix" for="uname"><b>Username</b></label>
-					<input className="w-full px-4 rounded-full clearfix" type="text" placeholder="Enter Username" name="uname" required />
-					<label className="w-screen clearfix" for="psw"><b>Password</b></label>
-					<input className="w-full px-4 rounded-full clearfix" type="password" placeholder="Enter Password" name="psw" required />
+			<div style={{height: '80vh', width: '80vw'}} className="mx-4 p-40 bg-blue-600 rounded-lg self-center text-center relative">
+				<div style={{height: '85%', width: '85%', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className="bg-blue-500 inline-block p-8 absolute flex-col items-center justify-center leading-8 border-2 border-blue-500 rounded-lg">
+					<label className="w-screen clearfix sm:text-lg md:text-xl lg:text-2xl xl:text-3xl" for="uname"><b>Username</b></label>
+					<input className="w-full p-2 m-2 rounded-full clearfix sm:text-lg md:text-xl lg:text-2xl xl:text-3xl" type="text" placeholder="Enter Username" name="uname" required />
+					<label className="w-screen clearfix sm:text-lg md:text-xl lg:text-2xl xl:text-3xl" for="psw"><b>Password</b></label>
+					<input className="w-full p-2 m-2 rounded-full clearfix sm:text-lg md:text-xl lg:text-2xl xl:text-3xl" type="password" placeholder="Enter Password" name="psw" required />
 					<Link to="/Authenticated">
 						<Button buttonText="Login" />
 					</Link>
