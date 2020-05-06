371821313b54acc3eb3dd1be02eb894e5276b9d6
zbedsole <zbedsole@myocv.com>
2020-05-06 13:31:57 -0500

    Made small changes to all files

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ export default function App() {
 	
 	return (
 		<div className="container flex h-screen w-screen justify-center text-center">
-			<div style={{height: '50vh', width: '50vw'}} className="mx-4 p-40 bg-blue-600 rounded-lg self-center text-center relative">
-				<div style={{height: '85%', width: '85%', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className="bg-blue-500 inline-block p-8 absolute flex flex-col items-center justify-center leading-8 border-2 border-blue-500 rounded-lg">
-					<label className="w-screen clearfix sm:text-md md:text-lg lg:text-xl xl:text-2xl" for="uname"><b>Username</b></label>
+			<div style={{height: '50vh', width: '50vw'}} className="mx-4 p-40 bg-blue-600 rounded-lg self-center relative">
+				<div style={{height: '85%', width: '85%', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className="bg-blue-500 inline-block p-8 absolute flex flex-col items-center justify-center leading-8 rounded-lg">
+					<label className="clearfix sm:text-md md:text-lg lg:text-xl xl:text-2xl" htmlFor="uname"><b>Username</b></label>
 					<input className="w-9/12 px-2 py-1 m-2 rounded-full clearfix sm:text-md md:text-lg lg:text-xl xl:text-2xl" type="text" placeholder="Enter Username" name="uname" required />
-					<label className="w-screen clearfix sm:text-md md:text-lg lg:text-xl xl:text-2xl" for="psw"><b>Password</b></label>
+					<label className="clearfix sm:text-md md:text-lg lg:text-xl xl:text-2xl" htmlFor="psw"><b>Password</b></label>
 					<input className="w-9/12 px-2 py-1 m-2 rounded-full clearfix sm:text-md md:text-lg lg:text-xl xl:text-2xl" type="password" placeholder="Enter Password" name="psw" required />
 					<Link to="/Authenticated">
 						<Button buttonText="Login" />
 					</Link>
 					<div className="block">
-						<input className="rounded-lg" type="checkbox" onClick={() => (setChecked(!checked))} checked={checked} name="remember" />
+						<input className="rounded-lg" type="checkbox" onChange={() => (setChecked(!checked))} checked={checked} name="remember" />
 						<label> Remember Me</label>
 					</div>
 				</div>
