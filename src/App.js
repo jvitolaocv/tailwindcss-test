ca0a6196286c6db471dedae4752c520031d0b3b0
jvitolaocv <jvitola@myocv.com>
2020-05-08 10:05:13 -0500

    Creatiing functioning login page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import './App.css';
 
 export default function App() {
 	const [checked, setChecked] = useState(true);
-	
-	return (
+			
+			return (
 		<div className="container flex h-screen w-screen justify-center text-align-center">
 			<div className="sm:h-10/12 sm:w-screen sm:mx-8 p-10 bg-blue-600 rounded-lg self-center text-center relative">
 				<div style={{top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className="sm:h-6/12 sm:w-auto bg-blue-500 flex justify-center inline-block mx-10 my-8 p-4 flex-col leading-8 border-2 border-blue-900 rounded-lg shadow-md">
+					<form method="post" action="login.php" onsubmit="return validate(this)">
 					<label className="w-screen clearfix" for="uname"><b>Username</b></label>
 					<input className="w-full px-4 rounded-full clearfix" type="text" placeholder="Enter Username" name="uname" required />
 					<label className="w-screen clearfix" for="psw"><b>Password</b></label>
 					<input className="w-full px-4 rounded-full clearfix" type="password" placeholder="Enter Password" name="psw" required />
+					</form>
 					<Link to="/Authenticated">
 						<Button buttonText="Login" />
+					</Link>
+					<Link to="/accountRegister">
+						<Button buttonText="Create Account"/>
 					</Link>
 					<div className="block">
 						<input className="rounded-lg" type="checkbox" onClick={() => (setChecked(!checked))} checked={checked} name="remember" />
---
+++ src/accountRegister.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function accountRegister(props) {
+   function createLogin(props) {
+
+        } 
+        
+        return(
+        
+        
+        <div className="container flex h-screen w-screen justify-center items-center">
+            <div className="h-auto w-auto p-8 bg-blue-600 rounded-lg self-auto text-center relative">
+                <div className="bg-blue-500 max-w-xs mx-auto rounded-lg justify-evenly">
+                </div>
+            </div>
+        </div>
+    )
+};
